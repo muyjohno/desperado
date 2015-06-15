904b7fd6f9f6e04013147864f3beb3c62ded612e
John O'Brien <john.obrien@1minus1.com>
2015-06-15 21:50:09 +0100

    Add login functionality

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+  attr_accessor :password
+  before_save :set_password
+
+  validates_presence_of :password, on: :create
+  validates_presence_of :username
+  validates_uniqueness_of :username
+
+  def encrypt_password(password, salt)
+    BCrypt::Engine.hash_secret(password, salt)
+  end
+
+  def set_password
+    return unless password.present?
+
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = encrypt_password(password, password_salt)
+  end
+
+  def authenticate?(password)
+    password_hash == encrypt_password(password, password_salt)
+  end
+
+  def self.authenticate(username, password)
+    user = find_by_username(username)
+    return user if user && user.authenticate?(password)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608200225) do
+ActiveRecord::Schema.define(version: 20150615191343) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "corp_id"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20150608200225) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
