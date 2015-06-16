d8c606212acc1ff244dc6f89e251e55d2fee4872
John O'Brien <john.obrien@1minus1.com>
2015-06-16 14:54:44 +0100

    Dismissible alerts and cleaner routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "leaderboard#show"
   get "jack-in" => "sessions#new", as: :login
+  post "jack-in" => "sessions#create", as: :do_login
   get "jack-out" => "sessions#destroy", as: :logout
-  resources :sessions, only: [:new, :create]
 end
