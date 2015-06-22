89e1954a5673e3fef7e8049de650f4914d583319
John O'Brien <john.obrien@1minus1.com>
2015-06-22 12:19:43 +0100

    Add player management

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :players, except: [:show, :new]
   root "leaderboard#show"
   get "jack-in" => "sessions#new", as: :login
   post "jack-in" => "sessions#create", as: :do_login
