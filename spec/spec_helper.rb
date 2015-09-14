1fadd9f174c619b42e50fcc57a24e8c23e736c63
John O'Brien <john.obrien@1minus1.com>
2015-09-14 14:55:18 +0100

    Add SOS to league table when it applies
    Write specs for `LeaderboardHelper`
    Namespace `LeaderboardHelper` from `spec/support` to avoid conflicts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
     end
   end
 
-  config.include LeaderboardHelper
+  config.include Spec::LeaderboardHelper
   config.include AchievementHelper
   config.include TiebreakerHelper
   config.include GameHelper
