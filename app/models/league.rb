1d3892905a2c50361234a41503ca973e2c24ec3e
John O'Brien <john.obrien@1minus1.com>
2015-09-17 06:57:10 +0100

    Change layout for consistency
    Also add "classic" theme

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,7 +2,7 @@ class League < ActiveRecord::Base
   INSTANCE_ID = 1
 
   has_many :rules
-  enum theme: [:default]
+  enum theme: [:default, :classic]
 
   def self.current
     return find(INSTANCE_ID) if exists?(INSTANCE_ID)
