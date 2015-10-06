e7a984d1a9c23d04e7ee89433d8b7cf0c1cddd86
John O'Brien <john.obrien@1minus1.com>
2015-10-06 15:05:42 +0100

    Display message when no games have been recorded

--- spec/views/leaderboard/show_spec.rb
+++ spec/views/leaderboard/show_spec.rb
@@ -1,27 +1,55 @@
 RSpec.describe "leaderboard/show.html.haml", type: :view do
   create_sample_league
 
-  before do
-    assign(:content, "Custom page content")
-    assign(:leaderboard, leaderboard.sorted_rows)
-    assign(:recent_games, Game.recent)
+  context "league under way" do
+    before do
+      assign(:content, "Custom page content")
+      assign(:leaderboard, leaderboard.sorted_rows)
+      assign(:recent_games, Game.recent)
 
-    render
+      render
+    end
+
+    it "should display page content" do
+      expect(rendered).to have_content("Custom page content")
+    end
+
+    it "should display leaderboard" do
+      expect(rendered).to have_content(t(:leaderboard_position))
+      expect(rendered).to have_content("Chuck")
+      expect(rendered).to have_content("3/1")
+    end
+
+    it "should display recent games" do
+      expect(rendered).to have_content(
+        "Chuck (Corp) played Ben (Runner): Chuck Win"
+      )
+    end
   end
 
-  it "should display page content" do
-    expect(rendered).to have_content("Custom page content")
-  end
+  context "no games" do
+    before do
+      assign(:content, "Custom page content")
+      assign(:leaderboard, [])
+      assign(:recent_games, [])
 
-  it "should display leaderboard" do
-    expect(rendered).to have_content(t(:leaderboard_position))
-    expect(rendered).to have_content("Chuck")
-    expect(rendered).to have_content("3/1")
-  end
+      render
+    end
 
-  it "should display recent games" do
-    expect(rendered).to have_content(
-      "Chuck (Corp) played Ben (Runner): Chuck Win"
-    )
+    it "should display page content" do
+      expect(rendered).to have_content("Custom page content")
+    end
+
+    it "should not display leaderboard" do
+      expect(rendered).not_to have_content(t(:leaderboard_position))
+    end
+
+    it "should not display recent games" do
+      expect(rendered).not_to have_content(t(:recent_games))
+    end
+
+    it "should display message" do
+      expect(rendered).to have_content(t(:no_games))
+    end
   end
 end
